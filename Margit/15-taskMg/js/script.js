d45ce9cb75c378503bd4cfad96058e6c5be92765
shresthapr <shresthaprajan38@gmail.com>
2020-09-27 17:52:59 +0300

    Jobs in finland page layout completed: effects and animation TODO

---
+++ Margit/15-taskMg/js/script.js
@@ -0,0 +1,7 @@
+var para = document.getElementsByClassName("search");
+console.log(para);
+para.addEventListener("click", showMore);
+function showMore() {
+	return searchbtn;
+}
+console.log("Something");
